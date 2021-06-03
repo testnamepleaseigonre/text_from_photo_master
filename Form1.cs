905077513fa17ad784c6d501a6537e221812b6e1
Valdemar Subotkovski <valdemar.subotkovski@gmail.com>
2021-06-03 16:00:04 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace text_from_photo_master
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                label1.Text = fileDialog.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sender sndr = new Sender(label1.Text);
+            sndr.Send();
+        }
+    }
+}
---
+++ Sender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace text_from_photo_master
+{
+    class Sender
+    {
+        private string fileName;
+        private string message;
+
+        public Sender(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Send()
+        {
+            IPAddress receiverIP = IPAddress.Parse("192.168.1.149"); 
+            IPEndPoint endPoint = new IPEndPoint(receiverIP, 2021); 
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+
+            byte[] fileNameBytes = Encoding.ASCII.GetBytes(Path.GetFileName(fileName));
+            byte[] fileNameLength = BitConverter.GetBytes(Path.GetFileName(fileName).Length);
+            byte[] fullBuffer = new byte[4 + fileNameBytes.Length];
+
+            fileNameLength.CopyTo(fullBuffer, 0); 
+            fileNameBytes.CopyTo(fullBuffer, 4);
+
+            socket.Connect(endPoint); 
+            socket.SendFile(fileName, fullBuffer, null, TransmitFileOptions.UseDefaultWorkerThread);
+            socket.Close();
+        }
+    }
+}
